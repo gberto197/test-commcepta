6e40609c83f4c683eb4d576a3fbf8501337f50b6
Gabriel Berto <gabrielkberto@gmail.com>
2019-06-06 12:40:45 -0300

    responsive navbar and add logo in navbar

--- src/views/partials/header.vue
+++ src/views/partials/header.vue
@@ -1,30 +1,51 @@
 <template>
   <div class="header">
-    <div>
-      <b-nav class="nav-bar">
-        <b-nav-item>EMPRESA</b-nav-item>
-        <b-nav-item>SERVIÇO</b-nav-item>
-        <b-nav-item>CONTATO</b-nav-item>
-      </b-nav>
+    <div class="nav-bar">
+      <b-container>
+        <b-navbar toggleable="lg" type="dark">
+          <b-navbar-brand href="#">
+            <img :src="require(`@/assets/images/logo.png`)" alt>
+          </b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+              <b-navbar-nav class="links-navbar">
+                <b-nav-item href="#" class="item-navbar">EMPRESA</b-nav-item>
+                <b-nav-item href="#" class="item-navbar">SERVIÇO</b-nav-item>
+                <b-nav-item href="#" class="item-navbar">CONTATO</b-nav-item>
+              </b-navbar-nav>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </b-container>
     </div>
     <!-- <div style="background-color:red;">
       <b-nav v-for="item in menuItems" :key="item.id">
         <b-nav-item>{{item.label}}</b-nav-item>
       </b-nav>
-    </div> -->
+    </div>-->
   </div>
 </template>
 
 <script>
-
-export default {
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
 .header {
   .nav-bar {
-    background-image: linear-gradient(to right, #4DD2D1, #0A2531);
+    background-image: linear-gradient(to right, #4dd2d1, #0a2531);
+    margin-bottom: 2rem;
+  }
+
+  .links-navbar {
+    font-weight: bold;
+  }
+
+  .item-navbar {
+    a {
+      color: white;
+    }
   }
 }
 </style>
