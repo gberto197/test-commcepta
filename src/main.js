6aadf07762fdc2a73a4377141f0122c5925cf911
Gabriel Berto <gabrielkberto@gmail.com>
2019-06-05 21:11:07 -0300

    add vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 Vue.config.productionTip = false
 
@@ -10,3 +15,5 @@ new Vue({
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
